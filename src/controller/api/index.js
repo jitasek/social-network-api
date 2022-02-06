88d67b64423fcffa6dc05f14177c9fe1d53774df
jitasek <jitka.moreira@gmail.com>
2022-02-06 23:37:49 +0100

    add and prep reaction and thought files to structure

--- src/controller/api/index.js
+++ src/controller/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user");
+//const thoughtRoutes = require("./user");
+//const reactionRoutes = require("./user");
 
 router.use("/users", userRoutes);
+//router.use("/thoughts", thoughtRoutes)
+//router.use("/reactions", reactionRoutes);
 
 module.exports = router;
