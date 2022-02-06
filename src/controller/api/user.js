81d2646f52e32990dc6a9d4d0a098a1b80983a75
jitasek <jitka.moreira@gmail.com>
2022-02-06 23:01:35 +0100

    add route get all users

--- src/controller/api/user.js
+++ src/controller/api/user.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 
 const { User } = require("../../models/");
 
-router.get("/", (req, res) => {
-  res.json({ msg: "You will get a list of users" });
+router.get("/", async (req, res) => {
+  const users = await User.find({});
+  res.json({ users });
 });
 
 router.post("/", async (req, res) => {
