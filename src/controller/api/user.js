a0bf453108463a63ac70f1a0644761f87ddf02e7
jitasek <jitka.moreira@gmail.com>
2022-02-06 22:59:52 +0100

    add route create user

--- src/controller/api/user.js
+++ src/controller/api/user.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
-//const { User } = require("../../models/");
+
+const { User } = require("../../models/");
 
 router.get("/", (req, res) => {
   res.json({ msg: "You will get a list of users" });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.json({ msg: "You will get a list of users" });
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      ...req.body,
+    });
+    res.json({ newUser, msg: "New user created" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong while creating the user.");
+  }
 });
 
 module.exports = router;
